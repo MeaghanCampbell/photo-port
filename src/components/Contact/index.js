13062c5170df883a2f5b3ed6a7567fba988b85e6
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-04-25 21:19:52 -0500

    handle form submits

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ContactForm() {
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    // destructure
+    const {name, email, message} = formState
+
+    //... is the spread operator so we can retail the other key value pairs in the form state object, without it we'd only get the name: value pair
+    // namd property of target refers to the name attribute of form element
+    // that attribute matches the formstate... [] allows us to create dynamic property names
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+
+    // handle for submits
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(formState)
+    }
+
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Name:</label>
-                <input type="text" name="name" />
+                <input type="text" defaultValue={name} onChange={handleChange} name="name"/>
             </div>
             <div>
                 <label htmlFor="email">Email address:</label>
-                <input type="email" name="email" />
+                <input type="email" defaultValue={email} onChange={handleChange} name="email"/>
             </div>
             <div>
                 <label htmlFor="message">Message:</label>
-                <textarea name="message" rows="5"  />
+                <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"/>
             </div>
                 <button type="submit">Submit</button>
             </form>
